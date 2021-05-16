22b629000cd61c654bd4d1a268af330fe589b12d
phiiil <phiiil3737@gmail.com>
2021-05-16 07:34:36 -0400

    display a list of blocks

--- src/lib/Blocklist/index.svelte
+++ src/lib/Blocklist/index.svelte
@@ -1,73 +1,89 @@
 <script>
 	console.log('Blocklist');
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { ethers } from 'ethers';
 
 	const provider = new ethers.providers.JsonRpcProvider({
 		url: 'https://mainnet.infura.io/v3/9b9f057582ab4e2bbb44024ba59e56f7'
 	});
 
+	let pageSize = 10;
 	let blockNumber;
+	const blocks = {};
+	const blocksPage = [];
 
-	provider.on('block', (bn) => {
-		console.log(`[event] block: ${bn}`);
-		blockNumber = bn;
-	});
+	// provider.on('block', (bn) => {
+	// 	console.log(`[event] block: ${bn}`);
+	// 	blockNumber = bn;
+	// });
+
+	function getBlocks() {
+		console.log(`Updating ${pageSize} blocks starting at ${blockNumber}...`);
+		for (let index = 0; index < pageSize; index++) {
+			let bn = blockNumber - index;
+			//console.log(`getting block #${bn}`);
+			let b = provider.getBlock(bn).then((b) => {
+				//console.log(`got block: ${bn}: ${JSON.stringify(b)}`);
+				blocks[bn] = b;
+				blocksPage.push(b);
+				blocksPage = blocksPage;
+			});
+		}
+	}
 
 	onMount(() => {
+		console.log(`on mount`);
 		provider.getBlockNumber().then((bn) => {
+			console.log(`latest block number: ${bn}`);
 			blockNumber = bn;
+			getBlocks();
 		});
+	});
+
+	onDestroy(() => {
+		console.log(`Removing event listeners`);
+		provider.off('block');
 	});
 </script>
 
-<div class="counter">
+<div class="">
+	<div>block number</div>
 	<div class="counter-viewport">
 		<div class="counter-digits">
 			<strong>{blockNumber}</strong>
 		</div>
 	</div>
+	<div class="blocklist">
+		<p>list {blocksPage.length}</p>
+		<ul>
+			{#each blocksPage as b}
+				<li>
+					{b.number}
+					<a target="_blank" href="">
+						{b.transactions.length}
+						{b.gasLimit}
+						{b.gasUsed}
+						{((b.gasUsed * 100) / b.gasLimit).toFixed(1)} %
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
 
 <style>
+	.blocklist {
+	}
 	.counter {
 		display: flex;
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		margin: 1rem 0;
 	}
-
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		color: var(--text-color);
-		font-size: 2rem;
-	}
-
-	.counter button:hover {
-		background-color: var(--secondary-color);
-	}
-
-	svg {
-		width: 25%;
-		height: 25%;
-	}
-
-	path {
-		vector-effect: non-scaling-stroke;
-		stroke-width: 2px;
-		stroke: var(--text-color);
-	}
-
 	.counter-viewport {
 		width: 28em;
 		height: 4em;
-		overflow: hidden;
+
 		text-align: center;
 		position: relative;
 	}

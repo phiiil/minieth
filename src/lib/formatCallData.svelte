3eef44b4fab59fb531f0f468444afc501efc6ade
phiiil <phiiil3737@gmail.com>
2021-05-22 12:24:18 -0400

    Using Etherscan API to decode calls

--- src/lib/formatCallData.svelte
+++ src/lib/formatCallData.svelte
@@ -8,44 +8,64 @@
 	let decodedDataObject;
 	let functionName;
 	let functionParams = [];
+	let errorMessage;
 
 	onMount(async () => {
 		console.log(`On Mount`);
-		const res = await fetch(`/static/0x7a250d5630b4cf539739df2c5dacb4c659f2488d.json`);
-		console.log(res);
-		const abi = await res.json();
-		if (callData && abi) {
-			//console.log(`abi: ${abi}`);
-			AbiDecoder.addABI(abi);
-			decodedDataObject = AbiDecoder.decodeMethod(callData);
-			functionName = decodedDataObject.name;
-			functionParams = decodedDataObject.params;
+		try {
+			let apiKey = import.meta.env.VITE_ETHERSCAN_APIKEY;
+			const res = await fetch(
+				`https://api.etherscan.io/api?module=contract&action=getabi&address=${to}&apikey=${apiKey}`
+			);
+			console.log(`res : ${res}`);
+			const abiResponse = await res.json();
+			if (abiResponse.status == 1) {
+				if (callData) {
+					const abi = JSON.parse(abiResponse.result);
+					console.log(`decoding abi of type: ${typeof abi}`);
+					AbiDecoder.addABI(abi);
+					decodedDataObject = AbiDecoder.decodeMethod(callData);
+					console.log(`decoded ${decodedDataObject}`);
+					functionName = decodedDataObject.name;
+					functionParams = decodedDataObject.params;
+				}
+			} else {
+				// no abi available
+				console.log(`abi: ${JSON.stringify(abiResponse)}`);
+				errorMessage = abiResponse.result;
+			}
+		} catch (e) {
+			console.log(e);
 		}
 	});
 </script>
 
 <div>
-	<span class="signature">
-		<span class="function">{functionName}</span> ({#each functionParams as p, i}
-			<span class="datatype">{p.type}</span>
-			{p.name}{#if i != functionParams.length - 1},&nbsp;{:else}){/if}
-		{/each}
-	</span>
-	<div class="callvalues">
-		<table>
-			<thead>
-				<tr> <th>param</th> <th>unit</th> <th>value</th></tr>
-			</thead>
-			{#each functionParams as p, i}
-				<tr>
-					<td>{p.name}</td>
-					<td>{p.type}</td>
-					<td>{p.value}</td>
-				</tr>
+	{#if errorMessage}
+		<span>{errorMessage}</span>
+	{:else}
+		<span class="signature">
+			<span class="function">{functionName}</span> ({#each functionParams as p, i}
+				<span class="datatype">{p.type}</span>
+				{p.name}{#if i != functionParams.length - 1},&nbsp;{:else}){/if}
 			{/each}
-		</table>
-	</div>
-	<!-- raw:<pre>{JSON.stringify(decodedDataObject, null, 2)} ) </pre> -->
+		</span>
+		<div class="callvalues">
+			<table>
+				<thead>
+					<tr> <th>param</th> <th>unit</th> <th>value</th></tr>
+				</thead>
+				{#each functionParams as p, i}
+					<tr>
+						<td>{p.name}</td>
+						<td>{p.type}</td>
+						<td>{p.value}</td>
+					</tr>
+				{/each}
+			</table>
+		</div>
+		<!-- raw:<pre>{JSON.stringify(decodedDataObject, null, 2)} ) </pre> -->
+	{/if}
 </div>
 
 <style>
